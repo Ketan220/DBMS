b6c71c7850f8712f9b579e10027c4e187709af45
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-22 19:09:31 +0530

    Add files via upload

---
+++ SQLQuery9.sql
@@ -0,0 +1,16 @@
+--5- create a table customer having columns custid,custname,custaddress
+create table customer(custid int primary key,custname varchar(10),custaddress varchar(20))
+
+--6- insert 10 records
+insert into customer values(1,'Gaurav','Chandramani Nagar')
+insert into  customer values(2,'Bhavesh','Ganesh Nagar')
+insert into  customer values(3,'Nayan','Gorewada Lake')
+insert into customer values(4,'Nidhi','rameshwari')
+insert into  customer values(5,'Vishal','Bajrang Nagar')
+insert into customer values(6,'Vinayal','Polic nagar')
+insert into  customer values(7,'Babu','Nandanwan')
+insert into  customer values(8,'Syali','Guruev Nagar')
+insert into  customer values(9,'Sans','Old Nandanwan')
+insert into customer values(10,'Rohit','Dighori')
+select*from customer
+
