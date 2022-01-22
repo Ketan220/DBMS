b94d8d1c152fdda6db0b1b4d5c353e58d60ae151
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-22 19:08:37 +0530

    Add files via upload

---
+++ SQLQuery5.sql
@@ -0,0 +1,12 @@
+--SQL Server CREATE TABLE example
+create table student(rno int,name varchar(20))
+
+--Query to insert record into student table
+insert into student values (1,'piyush')
+
+--Query to view all records from student table
+select * from student
+
+--To add another record in student
+insert into student values(2,'mayuri')
+select * from student
