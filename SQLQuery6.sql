20cde6a1d7ad951ed984cf9d820b7c65da996cce
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-22 19:08:48 +0530

    Add files via upload

---
+++ SQLQuery6.sql
@@ -0,0 +1,9 @@
+--Query to create a product table having columns productid , productname, price , quantity
+create table product (productid int,productname varchar(20),price int,quantity int)
+insert into product values (1,'computer',15000,12)
+insert into product values (2,'Mouse',500,2)
+insert into product values (3,'Keyboard',1500,12)
+insert into product values (4,'CPu',1500,10)
+
+select * from product
+
