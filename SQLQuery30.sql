e83a9f6420104bbb0db4739b1e1740eec0918381
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-27 18:57:24 +0530

    Add files via upload

---
+++ SQLQuery30.sql
@@ -0,0 +1,8 @@
+--SQL Server SELECT DISTINCT - distinct custname
+select distinct custname from product
+
+--SQL Server SELECT DISTINCT - distinct city
+select distinct city from product
+
+--SQL Server SELECT DISTINCT - distinct state
+select distinct state from product
