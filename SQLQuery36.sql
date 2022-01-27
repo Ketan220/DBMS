52e545a86f698cb84b523bedd9baa22f5273bd7c
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-27 19:00:02 +0530

    Add files via upload

---
+++ SQLQuery36.sql
@@ -0,0 +1,4 @@
+--SQL Server WHERE - Like operator
+--G) Finding rows whose values contain a string
+--The following example uses the LIKE operator to find products whose name contains the string Cruiser:
+select * from student where name like '%bh%'
