1028a305d47f265adf334e1930c0c68ad51f210a
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-27 18:58:19 +0530

    Add files via upload

---
+++ SQLQuery32.sql
@@ -0,0 +1,7 @@
+--Finding rows that meet two conditions
+select custname,city from product
+where custname='amit' and city='nagpur'
+
+
+select custname,state from product
+where custname='amit' and state='mh'
