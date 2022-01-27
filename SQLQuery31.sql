ab61622314bf449c5ea246a64b5506fa10d545e6
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-27 18:57:55 +0530

    Add files via upload

---
+++ SQLQuery31.sql
@@ -0,0 +1,8 @@
+--SQL Server WHERE examples
+select * from student where rno=1
+
+select * from student where rno>4
+
+select * from student where name='gaurav'
+
+select * from student where Address='Chandramani Nagar'
