edbfa81c2dcf0412d8d076ff0f30e1f3b838c8d9
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-01-31 16:12:32 +0530

    Add files via upload

---
+++ SQLQuery61.sql
@@ -0,0 +1,7 @@
+--1NF
+insert into employees values('bhavesh',47754548,'mh')
+insert into employees values('bhavesh',09585457,'mh')
+insert into employees values('kamlesh',75489548,'up')
+insert into employees values('rahul',848856865,'mp')
+insert into employees values('rahul',98746453,'mp')
+select* from employees
