c616fed1997f2687d45319a2f95a2748c0474b83
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-03 15:45:39 +0530

    Add files via upload

---
+++ SQLQuery86.sql
@@ -0,0 +1,24 @@
+/*create a table customer having custid,custname,telno
+create a table orders having orderid,orderdate,custid ,prodname,qty,rate
+then write a query to display records from customer and orders tables using inner join*/
+create table customer (custid int,custname varchar(30),telno int)
+
+create table orders (orderid int,orderdate int,custid int,prodname varchar(30),qty int,rate int,)
+
+insert into customer values (123,'rishi',90876467)
+insert into customer values (23,'ketan',85856868)
+insert into customer values (24,'bhavesh',56868788)
+insert into customer values(266,'sans',7689890)
+
+
+insert into orders values (1,22092022,23,'Mouse',8,1500)
+insert into orders values (2,09112020,45,'Keyboard',5,2000)
+insert into orders values (3,12092019,266,'cpu',2,4000)
+insert into orders values (4,12072016,45,'desktop',2,4435)
+
+select customer.custid,customer.custname,customer.telno,orders.orderdate,orders.prodname from customer 
+left outer join orders
+on customer.custid=orders.custid
+
+
+
