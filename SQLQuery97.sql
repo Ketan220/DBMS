08d99d197da9c8d4f8ed07d638a9378831d3a524
Ketan220 <95577873+Ketan220@users.noreply.github.com>
2022-02-04 19:55:49 +0530

    Add files via upload

---
+++ SQLQuery97.sql
@@ -0,0 +1,16 @@
+create table product(productid int,productname varchar(20),price int,qty int)
+
+insert into product values(1,'mouse',120,7),
+(2,'keyboard',220,7),
+(3,'desktop',12200,7),
+(4,'cpu',1300,2),
+(2,'keyboard',250,7),
+(1,'mouse',500,7);
+
+select * from product
+
+select productname,sum(price) 'total price' from product
+group by productname
+having min(qty)>=2
+
+
